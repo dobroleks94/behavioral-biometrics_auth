c1293e7ea28abd09188b7eba3dd5b0164592a5cf
Oleksii Dobroshtan <aleksey.dobroshtan3579@gmail.com>
2020-11-29 11:08:40 +0200

    Update AssociationRulesService.java

--- src/main/java/com/diploma/behavioralBiometricsAuthentication/services/AssociationRulesService.java
+++ src/main/java/com/diploma/behavioralBiometricsAuthentication/services/AssociationRulesService.java
@@ -60,7 +60,7 @@ public class AssociationRulesService {
         this.utility = new Utility();
     }
 
-    public void saveAll(List<AssociationRule> associationRules){ associationRules.parallelStream().forEach(associationRuleRepository::saveAndFlush); }
+    public void saveAll(List<AssociationRule> associationRules){ associationRuleRepository.saveAll(associationRules); }
     public void deleteAll() { associationRuleRepository.deleteAll(); }
     public List<AssociationRule> findAll() { return associationRuleRepository.findAll(); }
 
