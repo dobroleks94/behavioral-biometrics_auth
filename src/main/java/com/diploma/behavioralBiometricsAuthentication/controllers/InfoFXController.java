427801f781cea0b87822ecf3a1fb5cbfb16bdc32
dobroshtan <aleksey.dobroshtan@gmail.com>
2021-02-24 09:26:39 +0200

    - last update

--- src/main/java/com/diploma/behavioralBiometricsAuthentication/controllers/InfoFXController.java
+++ src/main/java/com/diploma/behavioralBiometricsAuthentication/controllers/InfoFXController.java
@@ -203,6 +203,7 @@ public class InfoFXController {
         phrase = phraseExtractor.getRandomPhrase();
         inputPhrase.setText(phrase);
         kpsService.clearAllContainers();
+        clearAllInputs();
         logger.log(SystemLogger.KEY_FEATURE_CONTAINERS_CLEAN);
     }
     private void updateInfoCard(){
