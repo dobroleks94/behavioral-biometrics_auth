34b0ee31b4bbd47815acfac091d2ee72266ea79f
dobroshtan <aleksey.dobroshtan3579@gmail.com>
2021-06-30 18:39:14 +0300

    - updated spmf library to their last release (26th of June, 2021) - fixed bugs in calculating ranges for VERY_LOW and VERY_HIGH measurements (processed 'min and max values are negative numbers' case)

--- src/main/java/com/diploma/behavioralBiometricsAuthentication/repositories/AssociationRuleRepository.java
+++ src/main/java/com/diploma/behavioralBiometricsAuthentication/repositories/AssociationRuleRepository.java
@@ -2,6 +2,8 @@ package com.diploma.behavioralBiometricsAuthentication.repositories;
 
 import com.diploma.behavioralBiometricsAuthentication.entities.associationRule.AssociationRule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AssociationRuleRepository extends JpaRepository<AssociationRule, Long> {
 }
