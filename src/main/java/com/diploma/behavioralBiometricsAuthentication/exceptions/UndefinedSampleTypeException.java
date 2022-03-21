da78a066f35acf273f8ebfe68f078b5f1b1a37d5
dobroshtan <aleksey.dobroshtan3579@gmail.com>
2022-03-21 08:07:22 +0200

    - upgraded jsoup version

--- src/main/java/com/diploma/behavioralBiometricsAuthentication/exceptions/UndefinedSampleTypeException.java
+++
@@ -1,8 +0,0 @@
-package com.diploma.behavioralBiometricsAuthentication.exceptions;
-
-public class UndefinedSampleTypeException extends RuntimeException {
-
-    public UndefinedSampleTypeException(String message) {
-        super("Undefined sample exception: " + message);
-    }
-}
